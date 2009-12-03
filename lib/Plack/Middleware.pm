ac9e6cc01fea61346e5c6afb6c9d06bb9232536e
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-12-03 08:52:20 +0000

    Import of MIYAGAWA/Plack-0.9018 from CPAN.
    gitpan-cpan-distribution: Plack
    gitpan-cpan-version:      0.9018
    gitpan-cpan-path:         MIYAGAWA/Plack-0.9018.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware.pm
+++ lib/Plack/Middleware.pm
@@ -8,6 +8,20 @@ use overload '&{}' => sub { shift->to_app(@_) }, fallback => 1;
 
 __PACKAGE__->mk_accessors(qw/app/);
 
+sub new {
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+
+    my $self;
+    if (@_ == 1 && ref $_[0] eq 'HASH') {
+        $self = bless {%{$_[0]}}, $class;
+    } else {
+        $self = bless {@_}, $class;
+    }
+
+    $self;
+}
+
 sub import {
     my $class = shift;
     if (@_) {
