aa403db64b2972bd062b4e6f1fc31d83fd69fcd2
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-01-06 02:44:35 +0000

    Import of MIYAGAWA/Plack-0.9028 from CPAN.
    gitpan-cpan-distribution: Plack
    gitpan-cpan-version:      0.9028
    gitpan-cpan-path:         MIYAGAWA/Plack-0.9028.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Plack/Util.pm
+++ lib/Plack/Util.pm
@@ -27,7 +27,7 @@ sub load_class {
 sub is_real_fh ($) {
     my $fh = shift;
 
-    my $reftype = Scalar::Util::reftype($fh);
+    my $reftype = Scalar::Util::reftype($fh) or return;
     if (   $reftype eq 'IO'
         or $reftype eq 'GLOB' && *{$fh}{IO}
     ) {
@@ -121,7 +121,7 @@ sub run_app($$) {
         $app->($env);
     } catch {
         my $body = "Internal Server Error";
-        $env->{'psgi.errors'}->print($@);
+        $env->{'psgi.errors'}->print($_);
         return [ 500, [ 'Content-Type' => 'text/plain', 'Content-Length' => length($body) ], [ $body ] ];
     }
 }
